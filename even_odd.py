733633fe736d2976b0a400a1ffa9d842e796f839
marina_v_sokolova <sokolovamv5@gmail.com>
2025-09-15 17:00:03 +0700

    Добавила функцию определения чётны или нечётных чисел

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+def even_or_odd(a):
+    if a % 2 == 0:
+        return print("Even")
+    else:
+        return print("Odd")
+even_or_odd(4)
+
+#альтернативное и более изящное решение задачи
+#def even_or_odd(number):
+#	return 'Odd' if number % 2 else 'Even'
